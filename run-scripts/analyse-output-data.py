70de639e8ad595c8ada18f0ad005cabad940bd52
Andrew Owenson <a.m.b.owenson@warwick.ac.uk>
2021-02-18 22:56:49 +0000

    Improve performance analysis script

--- run-scripts/analyse-output-data.py
+++ run-scripts/analyse-output-data.py
@@ -27,6 +27,7 @@ if not os.path.isdir(output_dirpath):
 ## Read in data
 op2_data = pd.read_csv(os.path.join(data_dirpath, "op2_performance_data.mean.csv"), keep_default_na=False)
 op2_data["loop time"] = op2_data["total time"] - op2_data["mpi time"]
+# op2_data.drop(columns=["total time"], inplace=True)
 
 perfdata_filepath = os.path.join(data_dirpath, "PerfData.mean.csv")
 if not os.path.isfile(perfdata_filepath):
@@ -48,7 +49,7 @@ else:
 	if "OFFCORE_RESPONSE_0:ANY_DATA:ANY_RESPONSE" in flux_papi_df["PAPI counter"].values:
 		flux_gb_df = flux_papi_df[flux_papi_df["PAPI counter"]=="OFFCORE_RESPONSE_0:ANY_DATA:ANY_RESPONSE"]
 		flux_gb_df.drop(columns=["PAPI counter"], inplace=True)
-		flux_gb_df["actual GB read"] = flux_gb_df["count"]*64/1e9
+		flux_gb_df["GB read"] = flux_gb_df["count"]*64/1e9
 		flux_gb_df.drop(columns=["count"], inplace=True)
 		op2_data = op2_data.merge(flux_gb_df, validate="many_to_one")
 
@@ -87,7 +88,7 @@ op2_data.loc[:,"kernel"].replace("unstructured_stream_kernel", "ustream", inplac
 op2_data = op2_data.rename(index=str, columns={"kernel":"loop", "mpi time":"sync"})
 
 # Calculate per-call times and counts:
-timers = ["total time", "sync"]
+timers = ["loop time", "sync"]
 for t in timers:
 	op2_data[t+".percall"] = (op2_data[t] / op2_data["count"]).round(8)
 # op2_data = op2_data.drop(columns=["count"])
@@ -95,35 +96,54 @@ for t in timers:
 bw_df = None
 if "flux_iters" in op2_data.columns.values:
 	# Calculate mesh bytes transferred:
-	## In one multigrid V-cycle, the ratio between total numbers of nodes and edges is:
-	# (a) Rotor37 mesh:
-	edges_per_node = 3.68
-	# (b) M6 wing mesh:
-	# edges_per_node = 4.04
+	# Need to know number of nodes in mesh, not currently exported by MG-CFD or OP2.
+	# Rather than code this, can instead infer it as only two meshes are currently 
+	# supported. 
+	# Determine whether M6-wing or some variant of Rotor37 to assume ratio of edges to nodes, 
+	# then apply to 'flux_iters' which is number of edges.
+	m6wing_nedges_MG_avg = 609529.25
 	op2_data["nedges"] = op2_data["flux_iters"]
+	op2_data["nedges/call"] = op2_data["nedges"] / op2_data["count"]
+	group_cols = [c for c in ["loop", "nranks"] if c in op2_data.columns.values]
+	op2_data_grp = op2_data.groupby(group_cols, as_index=False)
+	op2_data_grp_sum = op2_data_grp.sum()[["loop", "nranks", "nedges/call"]]
+	nedges = op2_data_grp_sum[op2_data_grp_sum["loop"]=="flux"].reset_index().loc[0,"nedges/call"]
+	## In one multigrid V-cycle, the ratio between total numbers of nodes and edges is:
+	if abs((nedges-m6wing_nedges_MG_avg)/m6wing_nedges_MG_avg) < 0.1:
+		# Assume M6 wing
+		print("- note: assuming M6-wing mesh to infer number of nodes")
+		edges_per_node = 4.04
+	else:
+		# Assume Rotor37 mesh
+		print("- note: assuming Rotor37 mesh to infer number of nodes")
+		edges_per_node = 3.68
+
 	op2_data["nnodes"] = (op2_data["nedges"]/edges_per_node).round()
-	op2_data["effective edges GB read"]  = ((op2_data["nedges"]*3  )/1e9*8).round(5)
-	op2_data["effective nodes GB read"]  = ((op2_data["nnodes"]*5*2)/1e9*8).round(5)
-	op2_data["effective nodes GB write"] = ((op2_data["nnodes"]*5  )/1e9*8).round(5)
-	op2_data["effective GB read"] = op2_data["effective edges GB read"] + op2_data["effective nodes GB read"]
-	op2_data["effective GB write"] = op2_data["effective nodes GB write"]
+	op2_data["edges effective GB read"]  = ((op2_data["nedges"]*3   *8)/1e9).round(5)
+	op2_data["nodes effective GB read"]  = ((op2_data["nnodes"]*5*2 *8)/1e9).round(5)
+	op2_data["nodes effective GB write"] = ((op2_data["nnodes"]*5   *8)/1e9).round(5)
+	op2_data["effective GB read"] = op2_data["edges effective GB read"] + op2_data["nodes effective GB read"]
+	op2_data["effective GB write"] = op2_data["nodes effective GB write"]
 	op2_data["effective GB"] = op2_data["effective GB read"] + op2_data["effective GB write"]
 
 	group_cols = [c for c in ["loop", "nranks"] if c in op2_data.columns.values]
 	op2_data_grp = op2_data.groupby(group_cols, as_index=False)
 	op2_data_grp_mean = op2_data_grp.mean()[["loop", "nranks", "loop time"]]
-	op2_data_grp_sum = op2_data_grp.sum()[["loop", "nranks", "effective GB"]]
+	op2_data_grp_sum = op2_data_grp.sum()[["loop", "nranks", "effective GB", "effective GB read"]]
 	gb_sec_data = op2_data_grp_mean.merge(op2_data_grp_sum, validate="one_to_one")
+	gb_sec_data["effective read GB/sec"] = (gb_sec_data["effective GB read"] / gb_sec_data["loop time"]).round(1)
 	gb_sec_data["effective GB/sec"] = (gb_sec_data["effective GB"] / gb_sec_data["loop time"]).round(1)
-	gb_sec_data.drop(columns=["effective GB", "loop time"], inplace=True)
+	gb_sec_data.drop(columns=["effective GB", "effective GB read", "loop time"], inplace=True)
 	bw_df = gb_sec_data
 
-	if "actual GB read" in op2_data.columns.values:
-		## PAPI counter for recording actual DRAM traffice can only capture reads, not writes.
+	if "GB read" in op2_data.columns.values:
+		# op2_data["actual GB"] = op2_data["actual GB read"]
+		## PAPI counter for recording actual DRAM traffic can only capture reads, not writes.
 		## But there is a solution.
 		##
 		## First, note that:
 		## actual GB read = effective GB read + wasted GB read
+		## wasted GB read = cacheline bytes never read when line is later evicted
 		##
 		## For edge data, accessed directly with unit stride, there should be zero wasted GB read, 
 		## so effective == actual.
@@ -131,34 +151,40 @@ if "flux_iters" in op2_data.columns.values:
 		## Then, note that:
 		## actual GB read = actual edges GB read + actual nodes GB read
 		##
-		## This gives us 'actual nodes GB read', without having to calculate ratio of cache misses etc.
+		## This gives us 'actual nodes GB read' without having to calculate ratio of cache misses etc.
 		##
-		## For each node, 10 doubles are read and 5 written, so can calculate 'actual nodes GB write':
-		op2_data["actual nodes GB read"] = op2_data["actual GB read"] - op2_data["effective edges GB read"]
-		op2_data["actual nodes GB write"] = op2_data["actual nodes GB read"] / 2.0
-		op2_data["actual GB"] = op2_data["effective edges GB read"] + op2_data["actual nodes GB read"] + op2_data["actual nodes GB write"]
+		## For each node, 10 doubles are read and 5 written, so can estimate 'actual nodes GB write':
+		op2_data["nodes GB read"] = op2_data["GB read"] - op2_data["edges effective GB read"]
+		op2_data["nodes estimated GB write"] = op2_data["nodes GB read"] / 2.0
+		# op2_data["nodes estimated GB write"] = op2_data["nodes effective GB write"]
+		op2_data["estimated GB"] = op2_data["edges effective GB read"] + op2_data["nodes GB read"] + op2_data["nodes estimated GB write"]
 
 		group_cols = [c for c in ["loop", "nranks"] if c in op2_data.columns.values]
 		op2_data_grp = op2_data.groupby(group_cols, as_index=False)
 		op2_data_grp_mean = op2_data_grp.mean()[["loop", "nranks", "loop time"]]
-		op2_data_grp_sum = op2_data_grp.sum()[["loop", "nranks", "actual GB"]]
+		op2_data_grp_sum = op2_data_grp.sum()[["loop", "nranks", "GB read", "estimated GB"]]
 		gb_sec_data = op2_data_grp_mean.merge(op2_data_grp_sum, validate="one_to_one")
-		gb_sec_data["actual GB/sec"] = (gb_sec_data["actual GB"] / gb_sec_data["loop time"]).round(1)
-		gb_sec_data.drop(columns=["actual GB", "loop time"], inplace=True)
+		gb_sec_data["read GB/sec"] = (gb_sec_data["GB read"] / gb_sec_data["loop time"]).round(1)
+		gb_sec_data["estimated GB/sec"] = (gb_sec_data["estimated GB"] / gb_sec_data["loop time"]).round(1)
+		gb_sec_data.drop(columns=["GB read", "estimated GB", "loop time"], inplace=True)
 		bw_df = bw_df.merge(gb_sec_data, validate="one_to_one")
 
-	op2_data.drop(columns=["flux_iters", "nedges", "nnodes"], inplace=True, errors='ignore')
-	op2_data.drop(columns=["effective edges GB read", "effective nodes GB read", "effective nodes GB write"], inplace=True, errors='ignore')
+	op2_data.drop(columns=["flux_iters", "nedges", "nnodes", "nedges/call"], inplace=True, errors='ignore')
+	op2_data.drop(columns=["edges effective GB read", "nodes effective GB read", "nodes effective GB write"], inplace=True, errors='ignore')
 	op2_data.drop(columns=["effective GB read", "effective GB write", "effective GB"], inplace=True, errors='ignore')
-	op2_data.drop(columns=["actual nodes GB read", "actual nodes GB write"], inplace=True, errors='ignore')
-	op2_data.drop(columns=["actual GB read", "actual GB"], inplace=True, errors='ignore')
+	op2_data.drop(columns=["nodes GB read", "nodes estimated GB write"], inplace=True, errors='ignore')
+	op2_data.drop(columns=["GB read", "estimated GB"], inplace=True, errors='ignore')
+
+	wanted_loops = ["flux", "ustream"]
+	f = bw_df["loop"].isin(wanted_loops)
+	bw_df = bw_df[f]
 
 op2_data.to_csv(os.path.join(output_dirpath, "op2_data.csv"), index=False)
 if not bw_df is None:
 	bw_df.to_csv(os.path.join(output_dirpath, "node_bw_performance.csv"), index=False)
 
 ## Calculate MPI % (per rank):
-op2_data["sync pct"] = op2_data["sync"] / op2_data["total time"]
+op2_data["sync pct"] = op2_data["sync"] / (op2_data["loop time"] + op2_data["sync"])
 # ... and write out:
 mpi_pct_filepath = os.path.join(output_dirpath, "mpi_pct_rankwise.csv")
 if not os.path.isfile(mpi_pct_filepath):
@@ -178,10 +204,11 @@ if not os.path.isfile(mpi_pct_filepath):
 
 flux_data = op2_data.loc[op2_data["loop"]=="flux"]
 ustream_data = op2_data.loc[op2_data["loop"]=="ustream"]
-wall_data = op2_data.loc[op2_data["loop"]=="WALLTIME"]
+wall_data = op2_data.loc[op2_data["loop"]=="WALLTIME"].copy()
+wall_data["total time"] = wall_data["loop time"] + wall_data["sync"]
 
-flux_with_ustream_data = flux_data.rename(index=str, columns={"total time":"flux time"})[["nranks", "rank", "flux time"]]
-flux_with_ustream_data = flux_with_ustream_data.merge(ustream_data.rename(index=str, columns={"total time":"ustream time"})[["nranks", "rank", "ustream time"]])
+flux_with_ustream_data = flux_data.rename(index=str, columns={"loop time":"flux time"})[["nranks", "rank", "flux time"]]
+flux_with_ustream_data = flux_with_ustream_data.merge(ustream_data.rename(index=str, columns={"loop time":"ustream time"})[["nranks", "rank", "ustream time"]])
 flux_with_ustream_data["flux_pct_ustream"] = flux_with_ustream_data["flux time"] / flux_with_ustream_data["ustream time"]
 
 if "nranks" in op2_data.columns:
@@ -266,15 +293,16 @@ if "nranks" in op2_data.columns:
 		fig = plt.figure(figsize=fig_dims)
 		x = "nranks"
 		y = "flux_pct_ustream"
-		fig.suptitle("{0} vs {1}".format(x, y))
+		ylabel = "compute_flux() / unstructured_stream()"
+		title = "scaling of " + ylabel
+		fig.suptitle(title)
 		ax = fig.add_subplot(1,1,1)
 		ax.set_xlabel(x)
-		ax.set_ylabel(y)
-		ax.set_ylim([0.0, 4.0])
+		ax.set_ylabel(ylabel)
+		ax.set_ylim([1.0, 4.0])
 		ax.set_xlim([flux_with_ustream_data[x].min()-8, flux_with_ustream_data[x].max()+16])
 		plt.xticks(np.arange(flux_with_ustream_data[x].min(), flux_with_ustream_data[x].max()+16, 16))
-		plt.plot(flux_with_ustream_data_grp.mean()[x], flux_with_ustream_data_grp.mean()[y], 'o-', label="compute_flux() pct. ustream()")
-		plt.legend()
+		plt.plot(flux_with_ustream_data_grp.mean()[x], flux_with_ustream_data_grp.mean()[y], 'o-')
 		png_filename = 'flux-scaling-pct-mem-bound.png'
 		png_filepath = os.path.join(output_dirpath, png_filename)
 		plt.savefig(png_filepath)
