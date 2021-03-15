d573e0b17cac63e048a5db716a1761756ec5954a
Archie Powell (RR) <awp@scyrus.hpsg.dcs.warwick.ac.uk>
2021-03-15 12:11:37 +0000

    CPX Version 1.5.2 - tweaked amount of interpolate

--- src/config.h
+++ src/config.h
@@ -135,7 +135,7 @@ inline void set_config_defaults() {
 
     conf.validate_result = false;
 
-    conf.num_cycles = 800;
+    conf.num_cycles = 1000;
 
     conf.partitioner = Partitioners::Parmetis;
     conf.partitioner_method = PartitionerMethods::NotSet;
--- src_op/coupler_config.h
+++ src_op/coupler_config.h
@@ -1,4 +1,4 @@
-int upd_freq = 810;/* in every x MG cycles, send data through CPX*/
-int mgcycles = 800;/* number of MG cycles */
-int conversion_factor = 10; //approx. how many MG cycles equals one production cycle - used to control interpolation routine
-int MUM = 0; //multi-unit mode - 0 is single unit, multi rank, 1 is multi unit
+int upd_freq = 260;/* in every x MG cycles, send data through CPX*/
+int mgcycles = 1000;/* number of MG cycles */
+int conversion_factor = 1; //approx. how many MG cycles equals one production cycle - used to control interpolation routine
+int MUM = 1; //multi-unit mode - 0 is single unit, multi rank, 1 is multi unit
