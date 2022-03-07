6d97432325e4aa547963cfd72b04d237e2a881b7
Istvan Reguly <regulyistvan@gmail.com>
2022-03-07 10:23:33 +0100

    patches for new OP2 structure

--- translate2op2.sh
+++ translate2op2.sh
@@ -25,7 +25,7 @@ function _main {
 	## To add thread timers, set OP_TIME_THREADS environment variable:
 	# OP_TIME_THREADS=1 python "${OP2_INSTALL_PATH}"/../translator/c/python/op2.py $files
 
-	python "${OP2_INSTALL_PATH}"/../translator/c/python/op2.py $files
+	python "${OP2_INSTALL_PATH}"/../translator/c/op2.py $files
 } 
 
 _main
